055071796588dcc399c8bea0a38124363ab3c5dc
Litlife <litlife.club@gmail.com>
2020-07-27 13:35:20 +0300

    jest config chnaged

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
 	// bail: 0,
 
 	// The directory where Jest should store its cached dependency information
-	cacheDirectory: "D:\\Temp\\1\\jest",
+	//cacheDirectory: "D:\\Temp\\1\\jest",
 
 	// Automatically clear mock calls and instances between every test
 	clearMocks: true,
